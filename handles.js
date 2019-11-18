6fcb1c6ebed45f9841bed78d4bdca863997cf9cc
PierreLouis98 <36404556+PierreLouis98@users.noreply.github.com>
2019-11-18 12:04:55 +0100

    Ajout de la fonction HandleServer

--- handles.js
+++ handles.js
@@ -1,6 +1,12 @@
- module.exports = {
-serverHandle: function (req, res) {...} 
-  }
+export function serverHandle(req, res) {
+  // Retrieve and print the current path
+  const path = url.parse(req.url).pathname;
+  console.log(path);
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(content);
+  res.end();
+}
 
   // Je ne sais pas pourquoi ça ne marche pas 
   // je ne sais pas ce que je dois faire avec le fichier json 
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var url = require('url');
 var querystring = require('querystring');
 
 
-var server = http.createServer(function(req, res) {
+var server = http.createServer(function(req, res)
+{
     var params = querystring.parse(url.parse(req.url).query);
     res.writeHead(200, {"Content-Type": "text/plain"});
     if ('prenom' in params && 'nom' in params) {
