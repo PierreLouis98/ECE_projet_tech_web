d231c29a025b874a6a81c76d230a24cec477dc86
Soryanas98 <soryanas98@gmail.com>
2019-11-18 12:17:41 +0100

    code
    le code base de l'app

---
+++ handles.js
@@ -0,0 +1,6 @@
+ module.exports = {
+serverHandle: function (req, res) {...} 
+  }
+
+  // Je ne sais pas pourquoi ça ne marche pas 
+  // je ne sais pas ce que je dois faire avec le fichier json 
---
+++ index.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+
+
+var server = http.createServer(function(req, res) {
+    var params = querystring.parse(url.parse(req.url).query);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    if ('prenom' in params && 'nom' in params) {
+        res.write('Hello ' + params['prenom'] + ' ' + params['nom']);
+    }
+    else {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.write('Erreur 404. Page not found ');
+      res.end();  
+    }
+    res.end();
+});
+server.listen(8080);
