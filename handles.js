f873f8e0d56ee0b9cedc9d2e7f8edfdb96576c7b
Soryanas98 <soryanas98@gmail.com>
2019-11-18 14:41:50 +0100

    final version
    finalisation du code

--- handles.js
+++ handles.js
@@ -1,15 +1,36 @@
-export function serverHandle(req, res) {
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
+//Necessary imports
+module.exports = {
 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    serverHandle : function (req, res) {
+        // Import a module
+        const http = require('http')
 
-  if ('name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
+        // Import Node url module
+        const url = require('url')
 
-  res.end();
+        // Import Node querystring module
+        const qs = require('querystring')
+    
+        const route = url.parse(req.url)
+        const path = route.pathname 
+        const params = qs.parse(route.query)
+
+        if (path !== '/hello' ) {
+            res.writeHead(404, {"Content-Type": "text/plain"});
+            res.write("404 Not found");
+            res.end();
+        }
+        
+        else {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            if (path === '/hello' && 'name' in params) {
+                res.write('Hello ' + params['name'])
+            }
+            else {
+                 res.write('Hello anonymous')
+                }
+            res.end();
+        }
+    }
 }
